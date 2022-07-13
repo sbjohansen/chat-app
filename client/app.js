f43add5ecfd324cbd4256b0d11068809c02a093e
Slawomir Johansen <baran@fastmail.com>
2022-07-13 17:54:37 +0200

    ADD gitignore, base server

--- client/app.js
+++ client/app.js
@@ -14,10 +14,14 @@ var messages = [];
 
 const login = (e) => {
     e.preventDefault();
-    userName = userNameInput.value;
-    loginForm.classList.remove('show');
-    messagesSection.classList.add('show');
-    addMessageForm.classList.add('show');
+    if (userNameInput.value.length > 0) {
+        userName = userNameInput.value;
+        loginForm.classList.remove('show');
+        messagesSection.classList.add('show');
+        addMessageForm.classList.add('show');
+    } else {
+        alert('Please enter a username');
+    }
 };
 
 const sendMessage = (e) => {
